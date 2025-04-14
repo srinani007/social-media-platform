998f547c78059e7d594911d13844ef31394e48ba
prasanth <kvsprasanth007@gmail.com>
2025-04-14 01:20:51 -0400

    Upload to github,

---
+++ src/main/java/com/prash/social_media_platform/SocialMediaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.prash.social_media_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prash/social_media_platform/SocialMediaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prash.social_media_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
